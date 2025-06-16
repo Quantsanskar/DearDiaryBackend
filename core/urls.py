b0be1f4fefa3f6f53b60f99ac98a38c0480c9959
Quantsanskar <sanskarsrdav@gmail.com>
2025-06-17 03:11:39 +0530

    Changes to be committed: 	modified:   core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,10 +4,9 @@ from django.core.management import call_command
 from django.http import HttpResponse
 
 def backup_db(request):
-    if request.method == 'POST':
-        call_command('backup_db')
-        return HttpResponse('Database backup initiated')
-    return HttpResponse('Method not allowed', status=405)
+    # Accept both GET and POST requests
+    call_command('backup_db')
+    return HttpResponse('Database backup initiated successfully')
 
 urlpatterns = [
     path('', views.index, name='index'),
