49f3275be9a7f6a7b1d06d05a3cb3a55f0bd7299
Quantsanskar <sanskarsrdav@gmail.com>
2025-06-16 16:32:17 +0530

    Changes to be committed: 	modified:   backend/settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -115,9 +115,32 @@ SIMPLE_JWT = {
 
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"
     "https://deardiaryforprincessandme.netlify.app",
+    "http://localhost:3000",  # For local development
+    "http://127.0.0.1:3000",  # For local development
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+
+
+CORS_ALLOWED_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
